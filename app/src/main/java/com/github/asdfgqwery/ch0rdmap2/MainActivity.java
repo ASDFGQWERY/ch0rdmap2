a8067e0eb4b1a40e096f640e81c05cd797fd1ded
ASDFGQWERY <qweryasdfg00@gmail.com>
2022-10-23 06:08:33 +0900

    Initial commit

---
+++ app/src/main/java/com/github/asdfgqwery/ch0rdmap2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.asdfgqwery.ch0rdmap2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
