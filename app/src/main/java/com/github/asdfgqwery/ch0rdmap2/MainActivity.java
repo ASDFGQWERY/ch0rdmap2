4c34ad1bf201a147cbbf152afdafeaf0e65fcde1
ASDFGQWERY <qweryasdfg00@gmail.com>
2022-10-23 18:19:27 +0900

    initial1

--- app/src/main/java/com/github/asdfgqwery/ch0rdmap2/MainActivity.java
+++ app/src/main/java/com/github/asdfgqwery/ch0rdmap2/MainActivity.java
@@ -13,10 +13,15 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private String sp1SelectedItem;
+    private String sp2SelectedItem;
 
 
     @Override
@@ -32,8 +37,46 @@ public class MainActivity extends AppCompatActivity {
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sp1.setAdapter(adapter1);
 
-        //Spinner spinner = (Spinner)parent;
-        String sp1SelectedItem = (String)sp1.getSelectedItem();
+        // リスナーを登録
+        sp1.setOnItemSelectedListener(new OnItemSelectedListener() {
+            //　アイテムが選択された時
+            @Override
+            public void onItemSelected(AdapterView<?> parent,
+                                       View view, int position, long id) {
+                Spinner sp1 = (Spinner)parent;
+                sp1SelectedItem = (String)sp1.getSelectedItem();
+
+                ImageView fret = findViewById(R.id.fret);
+                ImageView layer1 = findViewById(R.id.layer1);
+                AssetManager assets = getResources().getAssets();
+
+                //Fret
+                try (InputStream istream1 = assets.open(fr)){
+                    Bitmap bitmap = BitmapFactory.decodeStream(istream1);
+                    fret.setImageBitmap(bitmap);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+
+                String l1;
+                l1 = sp1SelectedItem+sp2SelectedItem+".png";
+
+                try (InputStream istream2 = assets.open(l1)){
+                    Bitmap bitmap = BitmapFactory.decodeStream(istream2);
+                    layer1.setImageBitmap(bitmap);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            //　アイテムが選択されなかった
+            public void onNothingSelected(AdapterView<?> parent) {
+                //
+            }
+        });
+
 
 
         Spinner sp2 = (Spinner) findViewById(R.id.spinner2);
@@ -41,34 +84,45 @@ public class MainActivity extends AppCompatActivity {
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sp2.setAdapter(adapter2);
 
-        String sp2SelectedItem = (String)sp2.getSelectedItem();
+        // リスナーを登録
+        sp2.setOnItemSelectedListener(new OnItemSelectedListener() {
+            //　アイテムが選択された時
+            @Override
+            public void onItemSelected(AdapterView<?> parent2,
+                                       View view, int position, long id) {
+                Spinner sp2 = (Spinner)parent2;
+                sp2SelectedItem = (String)sp2.getSelectedItem();
+
+                ImageView fret = findViewById(R.id.fret);
+                ImageView layer1 = findViewById(R.id.layer1);
+                AssetManager assets = getResources().getAssets();
+
+                //Fret
+                try (InputStream istream1 = assets.open(fr)){
+                    Bitmap bitmap = BitmapFactory.decodeStream(istream1);
+                    fret.setImageBitmap(bitmap);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+
+                String l1;
+                l1 = sp1SelectedItem+sp2SelectedItem+".png";
+
+                try (InputStream istream2 = assets.open(l1)){
+                    Bitmap bitmap = BitmapFactory.decodeStream(istream2);
+                    layer1.setImageBitmap(bitmap);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
 
 
-        ImageView fret = findViewById(R.id.fret);
-        ImageView layer1 = findViewById(R.id.layer1);
-        AssetManager assets = getResources().getAssets();
+            }
 
-        //Fret
-        try (InputStream istream1 = assets.open(fr)){
-            Bitmap bitmap = BitmapFactory.decodeStream(istream1);
-            fret.setImageBitmap(bitmap);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        //Layer1
-        String l1;
-        l1 = sp1SelectedItem+sp2SelectedItem+".png";
-
-        try (InputStream istream2 = assets.open(l1)){
-            Bitmap bitmap = BitmapFactory.decodeStream(istream2);
-            layer1.setImageBitmap(bitmap);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-
-
+            //　アイテムが選択されなかった
+            public void onNothingSelected(AdapterView<?> parent2) {
+                //
+            }
+        });
 
     }
 }
